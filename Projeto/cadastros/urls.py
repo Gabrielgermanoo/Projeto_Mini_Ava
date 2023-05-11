bc2526d7a8dd2e3f875877bfbba3cb1f748c91de
Gabriel Germano <gabriel.lucasgermano@gmail.com>
2023-05-11 20:10:30 -0300

    final commit

--- Projeto/cadastros/urls.py
+++ Projeto/cadastros/urls.py
@@ -10,23 +10,23 @@ urlpatterns = [
     path('cadastrar/classe/', ClasseCreate.as_view(), name="cadastrar-classe"),
     path('cadastrar/status/', StatusCreate.as_view(), name="cadastrar-status"),
     path('cadastrar/progressao/', ProgressaoCreate.as_view(), name="cadastrar-progressao"),
-    path('cadastrar/questao', CommentCreate.as_view(), name='cadastrar-questao'),
+    path('cadastrar/comentario', CommentCreate.as_view(), name='cadastrar-comentario'),
 
     path('editar/atividade/<int:pk>', AtividadeUpdate.as_view(), name='editar-atividade'),
     path('editar/classe/<int:pk>', ClasseUpdate.as_view(), name='editar-classe'),
     path('editar/status/<int:pk>', StatusUpdate.as_view(), name='editar-status'),
     path('editar/progressao/<int:pk>', ProgressaoUpdate.as_view(), name='editar-progressao'),
-    path('editar/questao/<int:pk>', CommentUpdate.as_view(), name='editar-questao'),
+    path('editar/comentario/<int:pk>', CommentUpdate.as_view(), name='editar-comentario'),
 
     path('excluir/atividade/<int:pk>', AtividadeDelete.as_view(), name= 'excluir-atividade'),
     path('excluir/status/<int:pk>', StatusDelete.as_view(), name= 'excluir-status'),
     path('excluir/classe/<int:pk>', ClasseDelete.as_view(), name= 'excluir-classe'),
     path('excluir/progressao/<int:pk>', ProgressaoDelete.as_view(), name= 'excluir-progressao'),
-    path('excluir/questao/<int:pk>', CommentDelete.as_view(), name='excluir-questao'),
+    path('excluir/comentario/<int:pk>', CommentDelete.as_view(), name='excluir-comentario'),
 
     path('listar/atividades/', AtividadeList.as_view(), name= 'listar-atividades'),
     path('listar/status/', StatusList.as_view(), name= 'listar-status'),
     path('listar/classes/', ClasseList.as_view(), name= 'listar-classes'),
     path('listar/progressao/', ProgressaoList.as_view(), name='listar-progressao'),
-    path('listar/questao/', CommentList.as_view(), name='listar-questao'),
+    path('listar/comentario/', CommentList.as_view(), name='listar-comentario'),
 ]
